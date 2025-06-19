f1d0cfc51d2153da9ecaa8098e35a87c4b1ab4bf
Kenth16 <kennynavarre97@gmail.com>
2025-06-19 16:36:54 +0200

    getMIME

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -5,17 +5,25 @@ std::string getMIME(std::string fd)
     std::string contentType;
 	if (fd.find(".html") != std::string::npos)
 		contentType = "text/html";
-	else if (fd.find(".jpg") != std::string::npos)
+	else if (fd.find(".jpg") != std::string::npos || fd.find(".jpeg") != std::string::npos)
 		contentType = "image/jpeg";
 	else if (fd.find(".png") != std::string::npos)
-		contentType = "icon/png";
+		contentType = "image/png";
 	else if (fd.find(".ttf") != std::string::npos)
 		contentType = "font/ttf";
 	else if (fd.find(".js") != std::string::npos)
-		contentType = "tet/javascript";
+		contentType = "text/javascript";
+	else if (fd.find(".gif") != std::string::npos)
+        return "image/gif";
+	else if (fd.find(".otf") != std::string::npos)
+        return "font/otf";
+    else if (fd.find(".svg") != std::string::npos)
+        return "image/svg+xml";
 	else if (fd.find(".css") != std::string::npos)
 		contentType = "text/css";
+    else if (fd.find(".ico") != std::string::npos)
+        return "image/x-icon";
 	else
-		contentType = "text/txt";
+		contentType = "text/plain";
     return contentType;
 }

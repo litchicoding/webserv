91fb6bb2628f3f09edad9504fb4d77458c57204e
Salome Deutsch <sdeutsch@student.42.fr>
2025-06-23 14:29:12 +0200

    new branch (from kenth)

--- src/Client_response.cpp
+++ src/Client_response.cpp
@@ -3,7 +3,7 @@
 struct stat st;
 
 void	Client::handleGet() {
-	if (access(URI.c_str(), F_OK) != 0)
+	if (access(_URI.c_str(), F_OK) != 0)
 	{
 		std::cout << RED "ACCESS OUUUUU" RESET << std::endl;
 		return(handleError(404));
@@ -12,17 +12,17 @@ void	Client::handleGet() {
 	// 	return(handleError(500));
 	// if (!S_ISREG(st.st_mode))
 	// 	return(handleError(404));
-	if (access(URI.c_str(), R_OK) != 0)
+	if (access(_URI.c_str(), R_OK) != 0)
 		return(handleError(403));
 	// if (HeadersCorrect("GET") != OK)
 	// 	handleError(400);
-	std::ifstream	file(URI.c_str());
+	std::ifstream	file(_URI.c_str());
 	if (!file.is_open())
 		return (handleError(500));
 	std::ostringstream	body;
 	body << file.rdbuf();
 	
-	std::string MIME = getMIME(URI);
+	std::string MIME = getMIME(_URI);
 	
 	std::cout << RED << "GET : Reussie !" << RESET << std::endl;
 	
@@ -36,24 +36,24 @@ void	Client::handleGet() {
 }
 
 void	Client::handlePost() {
-	if (access(URI.c_str(), F_OK | W_OK) != 0)
+	if (access(_URI.c_str(), F_OK | W_OK) != 0)
 		return(handleError(404)); // pas sur du code ici !
-	std::ofstream	outfile(URI.c_str());
+	std::ofstream	outfile(_URI.c_str());
 	if (!outfile.is_open())
 		return(handleError(500));
 	
-	outfile.write(body.c_str(), body.size());
+	outfile.write(_body.c_str(), _body.size());
 	outfile.close();
-	this->response = "HTTP/1.1 201 Created\r\n";
-	this->response += "Content-Length: 0\r\n";
-	this->response += "Connection: keep-alive\r\n\r\n";
+	this->_response = "HTTP/1.1 201 Created\r\n";
+	this->_response += "Content-Length: 0\r\n";
+	this->_response += "Connection: keep-alive\r\n\r\n";
 	return ;
 }
 
 void	Client::handleDelete() {
-	if (access(URI.c_str(), F_OK) != 0)
+	if (access(_URI.c_str(), F_OK) != 0)
 		return(handleError(404));
-	if (std::remove(URI.c_str()) != 0)
+	if (std::remove(_URI.c_str()) != 0)
 		return(handleError(500));
 		// headers
 	else
